0136093f6d56658e5af1fb567306fb152b9b13ac
Ashutosh Dave <ashutosh.dave@eternussolutions.com>
2020-09-08 20:50:21 +0530

    Initial Commit

---
+++ BatchWatermark/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BatchWatermark
+//
+//  Created by Ashutosh Dave on 08/09/20.
+//  Copyright © 2020 Ashutosh Dave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
